50071f11d958b28b288ac29a153173b5aa02da98
Anoop Chauhan <chauhananu0567@gmail.com>
2021-08-25 20:13:24 +0530

    📦 NEW: firebase config in production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBvDGCAAUX9q00ddvNUa-eO95xuhK2CM3U",
+    authDomain: "dvara-money.firebaseapp.com",
+    projectId: "dvara-money",
+    storageBucket: "dvara-money.appspot.com",
+    messagingSenderId: "590893597432",
+    appId: "1:590893597432:web:468a8f9bce59413407b10d"
+  }
 };
